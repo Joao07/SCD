85cde9d1a4d6a2e167046e4f6701268f12646527
Otavio Costa <otavio_slz15@hotmail.com>
2016-04-06 23:13:52 -0300

    Alterações Feitas

--- arquivos/arquivos/x9_software.sql
+++ arquivos/arquivos/x9_software.sql
@@ -2,24 +2,6 @@ CREATE DATABASE "X9" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'P
 
 connect "X9";
 
-CREATE TABLE curso (
-    id_curso serial NOT NULL primary key,
-    descricao_curso character varying(60)
-);
-
-CREATE TABLE disciplina (
-    id_disciplina serial NOT NULL primary key,
-    descricao_disciplina character(200),
-    carga_horaria integer
-);
-
-CREATE TABLE ocorrencia (
-    "idOcorrencia" serial NOT NULL primary key,
-    "dataOcorrencia" date,
-    observacao character(300),
-    "descricaoOcorrencia" character(500)
-);
-
 CREATE TABLE professor (
     id_professor serial NOT NULL primary key,
     nome character(200),
@@ -36,6 +18,16 @@ CREATE TABLE professor (
     telefone_fixo character(30)
 );
 
+CREATE TABLE curso (
+    id_curso serial NOT NULL primary key,
+    descricao_curso character varying(60)
+);
+
+CREATE TABLE disciplina (
+    id_disciplina serial NOT NULL primary key,
+    descricao_disciplina character(200),
+    carga_horaria integer
+);
 
 CREATE TABLE turma (
     id_turma serial NOT NULL primary key,
@@ -43,4 +35,39 @@ CREATE TABLE turma (
     turno_turma character varying(15),
     ano_turma integer
 );
+
+CREATE TABLE periodo (
+    id_periodo serial NOT NULL primary key,
+    id_disciplina integer,
+    id_curso integer,
+foreign key (id_disciplina)references disciplina (id_disciplina),
+foreign key (id_curso)references curso (id_curso)
+);
+
+CREATE TABLE horario (
+    "id_horario" serial NOT NULL primary key,
+    id_professor integer not null,
+    "id_turma" integer,
+    "id_disciplina" character(300),
+    "hora_inicio" character(20),
+
+"data" data,
+    "status" integer(500),
+
+foreign key (id_professor)references profedsor (id_professor)
+);
+
+
+CREATE TABLE ocorrencia (
+    "idOcorrencia" serial NOT NULL primary key,
+id_professor integer not null,
+    "dataOcorrencia" date,
+    observacao character(300),
+    "descricaoOcorrencia" character(500),
+foreign key (id_professor)references profedsor (id_professor)
+);
+
+
+
+
 
